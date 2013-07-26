77368b2d2e7fea585b26b5be7ef55f292a28b743
Aman Gupta <aman@tmm1.net>
2013-07-25 19:05:27 -0700

    always dial 9

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-
+class Buzzzzz < Sinatra::Base
+  get '/' do
+    content_type 'text/xml'
+    %{
+<Response>
+  <Say>Hello</Say>
+  <Play>http://www.dialabc.com/i/cache/dtmfgen/wavpcm8.300/9.wav</Play>
+  <Pause/>
+  <Play>http://www.dialabc.com/i/cache/dtmfgen/wavpcm8.300/9.wav</Play>
+</Response>
+    }
+  end
 end
+
+Buzzzzz.run!
